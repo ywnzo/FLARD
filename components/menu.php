262c5667c8ea95b5411cc63dd3873c0e8ff25e71
v01d10 <lopatkovy@gmail.com>
2025-03-28 16:20:12 +0100

    Init composer. Moved API call to the server script that is called from the client to hide API key etc.

--- components/menu.php
+++ components/menu.php
@@ -36,14 +36,7 @@ $pageName = ucfirst($pageName);
 </head>
 
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-WKVHFV67KC"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-WKVHFV67KC');
-</script>
+<?php include 'components/google_analytics.php'; ?>
 
 <body>
 
